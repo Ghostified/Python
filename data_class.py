b08d5c3ff025de41c20205d81286d804b79dac50
Ghostified <bransonallan@gmail.com>
2025-12-23 00:28:09 +0300

    dev can we pipe

--- data_class.py
+++ data_class.py
@@ -1,4 +1,4 @@
-from dataclass import dataclass
+from dataclasses import dataclass
 
 @dataclass
 class Person: 
@@ -8,3 +8,7 @@ class Person:
 
 person = Person(name="Branson", age=99, job="Enginer")
 print(person)
+
+#test for equivalence with data class
+person2 = Person(name="Allan", age=89, job="Engineer")
+print(person == person2)
